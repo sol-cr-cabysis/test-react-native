4670571ba3f6db91a432e6aa9d33c30bef532419
Sol Calderon <calderonsol81@gmail.com>
2024-10-21 17:48:28 -0600

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Camera, useCameraDevices} from "react-native-vision-camera";
+import {useEffect, useRef, useState} from "react";
 
 export default function App() {
+  const camera = useRef(null);
+  const devices = useCameraDevices();
+  const device = devices.back;
+  
+  const [showCamera, setShowCamera] = useState(false);
+  const [imageSource, setImageSource] = useState("");
+  
+  useEffect(() => {
+    async function getPermission
+  })
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>sol</Text>
       <StatusBar style="auto" />
     </View>
   );
